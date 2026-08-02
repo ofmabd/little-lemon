a8a8fdbca3fa76757aaa9038c7237d9400d0fbf2
ofmabd <ofmabd@gmail.com>
2026-08-02 13:25:37 -0400

    Semantic HTML Tags and added logo

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <p>© Little Lemon</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import logo from './logo.jpg';
+
+function Header() {
+    return (
+        <header>
+            <img src={logo} alt="Little Lemon Logo" />
+        </header>
+    );
+};
+
+export default Header;

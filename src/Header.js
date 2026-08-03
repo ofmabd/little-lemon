75add93a62f91be0e6f46ca6967f98df10b0e7ec
ofmabd <ofmabd@gmail.com>
2026-08-03 09:42:44 -0400

    More CSS changes and resuable variables

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,11 @@
-import logo from "./logo.png";
+import logo from './logo.png';
 
 function Header() {
-  return (
-    <img src={logo} alt="Little Lemon logo" />
-  );
+    return (
+        <div className="logo">
+            <img src={logo} alt="Little Lemon Logo" />
+        </div>
+    )
 }
 
 export default Header;
